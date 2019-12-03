b4b1621a16c42cfd47ac79076f81747a61d9ac48
HiroiImanishi <imanishi@lab3.kuis.kyoto-u.ac.jp>
2019-12-03 15:58:14 +0900

    add exposital

---
+++ expostal/lib/expostal.ex
@@ -0,0 +1,18 @@
+defmodule Expostal do
+  @moduledoc """
+  Documentation for Expostal.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Expostal.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ expostal/test/expostal_test.exs
@@ -0,0 +1,8 @@
+defmodule ExpostalTest do
+  use ExUnit.Case
+  doctest Expostal
+
+  test "greets the world" do
+    assert Expostal.hello() == :world
+  end
+end
